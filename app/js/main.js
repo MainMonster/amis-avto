06630f86b2cdb8796b8d5db8dd36be2e6a629b55
Juliya M <yerkina25@gmail.ru>
2022-12-29 14:22:06 +1000

    правки

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,11 @@ $(function () {
     fade: true,
   });
 });
+
+const btnShowMoreCategory = document.querySelector(".btn-more");
+const hiddenClass = document.querySelector(".category__list-hidden");
+const showClass = document.querySelector(".btn-hide");
+
+btnShowMoreCategory.addEventListener("click", function () {
+  hiddenClass.classList.toggle("category__list-hidden");
+});
