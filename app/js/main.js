c8e45095b23fb35a52f51f36076e1f01ec5b9861
Juliya M <yerkina25@gmail.ru>
2022-12-30 17:46:22 +1000

    fix2

--- app/js/main.js
+++ app/js/main.js
@@ -12,11 +12,9 @@ const btnShowMore = document.querySelector(".btn-more");
 const classHidden = document.querySelector(".category__list-hidden");
 
 btnShowMore.addEventListener("click", function () {
-  classHidden.classList.toggle("category__list-hidden");
-
-  //   if (classHidden.classList.toggle("category__list-hidden")) {
-  //     btnShowMore.textContent = "Показать контент";
-  //   } else {
-  //     btnShowMore.textContent = "Скрыть контент";
-  //   }
+  if (classHidden.classList.toggle("category__list-hidden")) {
+    btnShowMore.textContent = "Показать контент";
+  } else {
+    btnShowMore.textContent = "Скрыть контент";
+  }
 });
