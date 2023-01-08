4503abd6195c011ab63d1ba395d18b44ae033fdc
Juliya M <yerkina25@gmail.ru>
2023-01-08 11:37:21 +1000

    slider, section, scroll

--- app/js/main.js
+++ app/js/main.js
@@ -18,3 +18,22 @@ btnShowMore.addEventListener("click", function () {
     btnShowMore.textContent = "Скрыть контент";
   }
 });
+
+$(document).ready(function () {
+  $("#menu").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+});
